175f50be3ec038d094482b35cbe38aa8ca228305
Tanishcq Mehta <tanishcqmehta@gmail.com>
2022-02-23 16:08:37 +0530

    initialized project

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const request = require("request");
+
+const app = express();
+
+
+
+app.listen(3000, ()=>{
+    console.log("server live at 3000");
+})
