43e75d3b94491919dc7df2af30bbeee5b1da15b2
Tanishcq Mehta <tanishcqmehta@gmail.com>
2022-02-23 17:35:49 +0530

    route setup for "/"

--- app.js
+++ app.js
@@ -4,7 +4,20 @@ const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/signup.html"); 
+})
+
+app.post("/", (req, res) => {
+    var firstname = req.body.firstName;
+    var lastname = req.body.lastName;
+    var email = req.body.email;
+
+    console.log(firstname, lastname, email);
+})
 
 app.listen(3000, ()=>{
     console.log("server live at 3000");
