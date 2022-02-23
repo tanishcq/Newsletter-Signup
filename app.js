9bb67feddd59ca309d439ba689a45e6b66d92b18
Tanishcq Mehta <tanishcqmehta@gmail.com>
2022-02-23 21:51:10 +0530

    added compatibility for Heroku

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post("/failure", (req, res) => {
     res.redirect("/");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("server live at 3000");
 })
 
